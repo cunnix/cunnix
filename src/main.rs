2014a917185d09a5b6a9c325944eac54e08c501e
Yuuki Rika <yvvki@duck.com>
2023-07-13 14:23:00 +0700

    Panic implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 #![no_std] // don't link the Rust standard library
 
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
 fn main() {}

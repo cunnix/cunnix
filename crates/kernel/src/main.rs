6035fd2b6f1583e6bced7fab8946cd26f3f2a6fb
Yuuki Rika <yvvki@duck.com>
2023-07-14 03:43:33 +0700

    Hello, Blue Kernel!

--- crates/kernel/src/main.rs
+++ crates/kernel/src/main.rs
@@ -1,9 +1,21 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+use bootloader_api::*;
+use bootloader_x86_64_common::logger::LockedLogger;
+use conquer_once::spin::OnceCell;
 use core::panic::PanicInfo;
 
-use bootloader_api::*;
+pub(crate) static LOGGER: OnceCell<LockedLogger> = OnceCell::uninit();
+pub(crate) fn init_logger(framebuffer_info: &'static mut info::FrameBuffer) {
+    let info = framebuffer_info.info();
+    let framebuffer = framebuffer_info.buffer_mut();
+
+    let logger = LOGGER.get_or_init(move || LockedLogger::new(framebuffer, info, true, false));
+    log::set_logger(logger).unwrap();
+    log::set_max_level(log::LevelFilter::Trace);
+    log::info!("Hello, Blue Kernel!");
+}
 
 /// This function is called on panic.
 #[panic_handler]
@@ -14,11 +26,9 @@ fn panic(_info: &PanicInfo) -> ! {
 entry_point!(main);
 
 fn main(info: &'static mut BootInfo) -> ! {
-    if let Some(framebuffer) = info.framebuffer.as_mut() {
-        for byte in framebuffer.buffer_mut() {
-            *byte = 0x00;
-        }
-    }
+    let framebuffer = info.framebuffer.as_mut().unwrap();
+
+    init_logger(framebuffer);
 
     loop {}
 }

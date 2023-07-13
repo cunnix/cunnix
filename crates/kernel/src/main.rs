831c3390e3e1b770395ebd4f2089093c2ca46332
Yuuki Rika <yvvki@duck.com>
2023-07-14 03:22:49 +0700

    Use `use` keywords

--- crates/kernel/src/main.rs
+++ crates/kernel/src/main.rs
@@ -1,14 +1,19 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+use core::panic::PanicInfo;
+
+use bootloader_api::*;
+
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-bootloader_api::entry_point!(main);
-fn main(info: &'static mut bootloader_api::BootInfo) -> ! {
+entry_point!(main);
+
+fn main(info: &'static mut BootInfo) -> ! {
     if let Some(framebuffer) = info.framebuffer.as_mut() {
         for byte in framebuffer.buffer_mut() {
             *byte = 0x00;
